8addfff0eea0340bde8270f77db9f0e42c48528b
yanguangjie <yanguangjie@bytedance.com>
2021-02-07 18:47:01 +0800

    fix: get correct rslv dir

--- cliresolve/__init__.py
+++ cliresolve/__init__.py
@@ -2,8 +2,13 @@
 CLI-Resolve(rslv): Make and resolve alias in CLI.
 
 """
+import os
+from pathlib import Path
+
+PWD = os.path.dirname(os.path.realpath(__file__))
+RSLV_DIR = os.path.abspath(os.path.join(PWD, os.pardir))
 
 __version__ = "0.0.1"
 __author__ = "sulirc"
 __delimiter__ = " => "
-__cache_file__ = ".rslv.db"
+__cache_file__ = RSLV_DIR + "/.rslv.db"
